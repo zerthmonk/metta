08e11f4c5490e090fcc56359f4ab1e5a486449a5
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2021-01-30 21:55:26 +0200

    photo styling

---
+++ front/src/components/UserPhoto.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="user-photo" :style=image></div>
+</template>
+
+<script>
+export default {
+  props: {
+    uid: {
+      type: Number,
+    },
+    size: {
+      type: String,
+      default: '50px'
+    }
+  },
+
+  computed: {
+    image() {
+      return `
+        background-image: url("/dist/${this.uid}.jpg");
+        width: ${this.size};
+        height: ${this.size};
+      `
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  @import '../assets/mixins'
+
+  .user-photo
+    background: beige
+    background: url({{uid}})
+    background-size: contain
+    @include border-radius(5px)
+
+</style>
